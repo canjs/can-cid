2c2b27564f77783a017df1364cf03c8a266c3cdd
Kevin Phillips <kphillips86@gmail.com>
2017-01-18 11:42:57 -0600

    1.0.1

---
+++ dist/amd/can-cid.js
@@ -0,0 +1,17 @@
+/*can-cid@1.0.0#can-cid*/
+define(function (require, exports, module) {
+    var namespace = require('can-namespace');
+    var _cid = 0;
+    var cid = function (object, name) {
+        if (!object._cid) {
+            _cid++;
+            object._cid = (name || '') + _cid;
+        }
+        return object._cid;
+    };
+    if (namespace.cid) {
+        throw new Error('You can\'t have two versions of can-cid, check your dependencies');
+    } else {
+        module.exports = namespace.cid = cid;
+    }
+});
