11da7ce53c2a62a63698fd29aeb7a3bf759da686
imasustink <austin@austinkurpuis.com>
2017-09-27 16:06:56 -0700

    1.1.0

---
+++ dist/amd/can-cid.js
@@ -0,0 +1,29 @@
+/*can-cid@1.0.3#can-cid*/
+define([
+    'require',
+    'exports',
+    'module',
+    'can-namespace'
+], function (require, exports, module) {
+    var namespace = require('can-namespace');
+    var _cid = 0;
+    var domExpando = 'can' + new Date();
+    var cid = function (object, name) {
+        var propertyName = object.nodeName ? domExpando : '_cid';
+        if (!object[propertyName]) {
+            _cid++;
+            object[propertyName] = (name || '') + _cid;
+        }
+        return object[propertyName];
+    };
+    cid.domExpando = domExpando;
+    cid.get = function (object) {
+        var propertyName = object.nodeName ? domExpando : '_cid';
+        return object[propertyName];
+    };
+    if (namespace.cid) {
+        throw new Error('You can\'t have two versions of can-cid, check your dependencies');
+    } else {
+        module.exports = namespace.cid = cid;
+    }
+});
